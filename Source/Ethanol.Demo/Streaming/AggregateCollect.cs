ea02aa0241bc95d49a53abd69238bef8c56ea062
Ondrej Rysavy <rysavy.ondrej@gmail.com>
2021-08-14 14:22:10 +0200

    Add Collect method to Aggregate framework.

--- Source/Ethanol.Demo/Streaming/AggregateCollect.cs
+++ Source/Ethanol.Demo/Streaming/AggregateCollect.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.StreamProcessing.Aggregates;
 using System.Linq.Expressions;
 using Microsoft.StreamProcessing;
+using System.Collections.Immutable;
 
 namespace Ethanol.Demo.Streaming
 {
@@ -21,25 +22,32 @@ namespace Ethanol.Demo.Streaming
             list.Remove(x);
             return list;
         }
-        class _Aggregate<TElement> : IAggregate<TElement, List<TElement>, TElement[]>
+        class _Aggregate<TElement> : IAggregate<TElement, ImmutableList<TElement>, TElement[]>
         {
-            public Expression<Func<List<TElement>, long, TElement, List<TElement>>> Accumulate() =>
-                (state, timestamp, item) => AddItem(state, item);
+            public Expression<Func<ImmutableList<TElement>, long, TElement, ImmutableList<TElement>>> Accumulate() =>
+                (state, timestamp, item) => state.Add(item);
 
-            public Expression<Func<List<TElement>, TElement[]>> ComputeResult() => l => l.ToArray();
+            public Expression<Func<ImmutableList<TElement>, TElement[]>> ComputeResult() => l => l.ToArray();
 
-            public Expression<Func<List<TElement>, long, TElement, List<TElement>>> Deaccumulate() =>
-                (state, timestamp, item) => RemoveItem(state, item);
+            public Expression<Func<ImmutableList<TElement>, long, TElement, ImmutableList<TElement>>> Deaccumulate() =>
+                (state, timestamp, item) => state.Remove(item);
 
-            public Expression<Func<List<TElement>, List<TElement>, List<TElement>>> Difference()
-            {
-                throw new NotImplementedException();
-            }
+            public Expression<Func<ImmutableList<TElement>, ImmutableList<TElement>, ImmutableList<TElement>>> Difference() =>
+                (left, right) => left.RemoveRange(right);
 
-            public Expression<Func<List<TElement>>> InitialState() => () => new List<TElement>();
+            public Expression<Func<ImmutableList<TElement>>> InitialState() => () => ImmutableList<TElement>.Empty;
 
         }
-        public static IAggregate<TSource, List<TResult>, TResult[]> CollectFlows<TKey,TSource,TResult>(
+        /// <summary>
+        /// Collects objects in a streamable.
+        /// </summary>
+        /// <typeparam name="TKey">The key of the stream.</typeparam>
+        /// <typeparam name="TSource">The type of source objects.</typeparam>
+        /// <typeparam name="TResult">The type of results.</typeparam>
+        /// <param name="window">The source window consisting of objects.</param>
+        /// <param name="selector">Function to project from <typeparamref name="TSource"/> to <typeparamref name="TResult"/> objects.</param>
+        /// <returns>An array of object collected in the given <paramref name="window"/>.</returns>
+        public static IAggregate<TSource, ImmutableList<TResult>, TResult[]> Collect<TKey,TSource,TResult>(
             this Window<TKey, TSource> window, Expression<Func<TSource, TResult>> selector)
         {
             var aggregate = new _Aggregate<TResult>();

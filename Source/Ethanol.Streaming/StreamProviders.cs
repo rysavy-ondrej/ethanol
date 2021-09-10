a556a0104218840f319f49c262aa0157736947af
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2021-09-10 14:17:59 +0200

    Demo for TOR detection using context-based analysis.

--- Source/Ethanol.Streaming/StreamProviders.cs
+++ Source/Ethanol.Streaming/StreamProviders.cs
@@ -2,6 +2,8 @@
 using Ethanol.Artifacts;
 using Ethanol.Providers;
 using Microsoft.StreamProcessing;
+using System;
+
 namespace Ethanol.Streaming
 {
     public static class StreamProviders
@@ -11,7 +13,7 @@ namespace Ethanol.Streaming
         /// </summary>
         public static IStreamable<Empty, TArtifact> GetStreamable<TArtifact>(this IArtifactProvider<TArtifact> artifactProvider) where TArtifact : Artifact
         {
-            return artifactProvider.GetObservable<TArtifact>().ToTemporalStreamable(x => x.StartTime, x => x.EndTime, disorderPolicy: DisorderPolicy.Drop());
+            return artifactProvider.GetObservable<TArtifact>().ToTemporalStreamable(x => x.StartTime, x => x.EndTime, disorderPolicy: DisorderPolicy.Adjust(TimeSpan.FromMinutes(5).Ticks));
         }
 
         /// <summary>
@@ -19,7 +21,7 @@ namespace Ethanol.Streaming
         /// </summary>
         public static IStreamable<Empty, TArtifact> GetStreamable<TArtifact>(this IArtifactProvider artifactProvider) where TArtifact : Artifact
         {
-            return artifactProvider.GetObservable<TArtifact>().ToTemporalStreamable(x => x.StartTime, x => x.EndTime, disorderPolicy: DisorderPolicy.Drop());
+            return artifactProvider.GetObservable<TArtifact>().ToTemporalStreamable(x => x.StartTime, x => x.EndTime, disorderPolicy: DisorderPolicy.Adjust(TimeSpan.FromMinutes(5).Ticks));
         }
     }
 }

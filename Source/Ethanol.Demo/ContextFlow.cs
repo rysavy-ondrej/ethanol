9fb36120f3a69de2b436f8a0123de2016eaac185
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2021-10-07 21:58:49 +0200

    Update ContextFlow.cs

--- Source/Ethanol.Demo/ContextFlow.cs
+++ Source/Ethanol.Demo/ContextFlow.cs
@@ -1,5 +1,6 @@
 ﻿using Ethanol.Streaming;
 using Microsoft.StreamProcessing;
+using Microsoft.StreamProcessing.Aggregates;
 using System;
 using System.Linq;
 using System.Linq.Expressions;
@@ -57,17 +58,27 @@ namespace Ethanol.Demo
             Func<Source1, Source2, Source3, Target> mergeFunc)
             where Source1 : class where Source2 : class where Source3 : class 
         {
+            /*
+             *             
+            // The problem here is that it JOIN generates a lot of intermediate results :(
             var merged = source1.Multicast(master =>
             {
                 var left = master.Join(source2, left => left.Flow, right => right.Flow, (left, right) => new ContextPair<Source1, Source2>(left.Flow, left.Context, right.Context));
                 var right = master.Join(source3, left => left.Flow, right => right.Flow, (left, right) => new ContextPair<Source1, Source3>(left.Flow, left.Context, right.Context));
                 return left.Join(right, left => left.Flow, right => right.Flow,(left, right) => new ContextFlow<Target>(left.Flow, mergeFunc(left.Left, left.Right, right.Right)));
             });
-            //
-            // The problem here is that it generates a lot of intermediate results :(
-            // Perhaps the problem is because of using Collect aggregates for generating the objects.
-            // TODO: Get the biggest joined item
-            return merged;
+            */
+            // the workaround is to use union and group:
+            var union = source1.Select(m => new { Flow = m.Flow, Item1 = m.Context,         Item2 = default(Source2),   Item3 = default(Source3) })
+                 .Union(source2.Select(m => new { Flow = m.Flow, Item1 = default(Source1),  Item2 = m.Context,          Item3 = default(Source3) }))
+                 .Union(source3.Select(m => new { Flow = m.Flow, Item1 = default(Source1),  Item2 = default(Source2),   Item3 =  m.Context }));
+            return union.GroupAggregate(
+                key => key.Flow,
+                agg1 => agg1.Where(x => x.Item1 != default).CollectList(x => x.Item1),
+                agg2 => agg2.Where(x => x.Item2 != default).CollectList(x => x.Item2),
+                agg3 => agg3.Where(x => x.Item3 != default).CollectList(x => x.Item3),
+                (key, val1, val2, val3) => new ContextFlow<Target>(key.Key, mergeFunc(val1.FirstOrDefault(), val2.FirstOrDefault(), val3.FirstOrDefault())));     
+            // better but some information is missing...error either here or in EXPAND....
         }
     }
 }

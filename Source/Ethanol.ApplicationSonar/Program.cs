261230d18eaa46d0a12153ccef6171f1d20727a2
Ondrej Rysavy <rysavy-ondrej@users.noreply.github.com>
2023-07-12 13:23:51 +0200

    Update Program.cs

--- Source/Ethanol.ApplicationSonar/Program.cs
+++ Source/Ethanol.ApplicationSonar/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// This is simple:
+// Read input JSON perform searching for IPs, kewords in domain names, etc, and produce the output...
+//
+// The Netify is provided as input XXX file and represented as in-memory database.
+// The app fingeprint is also loaded from XXX file and represented as in-memory database NMemory (???)
+// 

340510e0c056a91bbfdfbe4ef0758a0188fd670f
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2023-12-18 19:10:02 +0100

    Update test data.

--- TestData/test.ps1
+++ TestData/test.ps1
@@ -1,27 +1,27 @@
 Write-Host  "Building the solution.." -ForegroundColor Yellow -BackgroundColor DarkGreen
 Write-Host
 
-dotnet build ../../Source/Ethanol.Cli/Ethanol.Cli.csproj
+dotnet build ../Source/Ethanol.Cli/Ethanol.Cli.csproj
 
-$ethanolExe = "../../Source/Ethanol.Cli/bin/Debug/net7.0/ethanol"
+$ethanolExe = "$PWD/../Source/Ethanol.Cli/bin/Debug/net7.0/ethanol"
 
 Write-Host "Running tests in ContextBuilder" -ForegroundColor Yellow -BackgroundColor DarkGreen
 Write-Host
 
 cd ContextBuilder
-test.ps1
+./test.ps1
 cd ..
 
 Write-Host "Running tests in ContextProvider" -ForegroundColor Yellow -BackgroundColor DarkGreen
 Write-Host
 
 cd ContextProvider
-test.ps1
+./test.ps1
 cd ..
 
 Write-Host "Running tests in MalwareSonar" -ForegroundColor Yellow -BackgroundColor DarkGreen
 Write-Host
 
 cd MalwareSonar
-test.ps1
+./test.ps1
 cd ..

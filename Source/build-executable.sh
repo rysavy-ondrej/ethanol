df87ab1ac9ef7876ea7f253b0db804ef2f308d92
Ondrej Rysavy <rysavy@fit.vutbr.cz>
2024-01-22 11:03:07 +0100

    Update build-executable.sh

--- Source/build-executable.sh
+++ Source/build-executable.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
+mkdir ../../Deploy/Bin/
 cd Ethanol.Cli
-dotnet publish -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -c Release
-cp ./bin/Release/net7.0/win-x64/publish/ethanol.exe ../../Deploy/Bin/
-
 dotnet publish -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -c Release
 cp ./bin/Release/net7.0/linux-x64/publish/ethanol ../../Deploy/Bin/
-
-
- 
